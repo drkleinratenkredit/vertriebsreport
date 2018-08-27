da1806326e38d4e4069348044f1ec646480843ec
VolkerUndDrKlein <volker.gayk@drklein.de>
2018-08-27 08:30:29 +0200

    und weiter

--- r_code/testpiste.R
+++ r_code/testpiste.R
@@ -9,3 +9,12 @@ write.table(antrag,paste0(getwd(),"/output/antrag",Sys.Date(),".txt"), sep = "\t
 write.table(vorgang,paste0(getwd(),"/output/vorgang",Sys.Date(),".txt"),sep = "\t",row.names = FALSE)
 
 write.table(ds1,paste0(getwd(),"/output/antrag_vorgang",Sys.Date(),".txt"),sep = "\t",row.names = FALSE)
+
+ds2 <- ds2 %>% 
+  filter(VorgangAngelegtAmDatum > "2016-12-31")
+
+write.table(ds2,paste0(getwd(),"/output/antrag_vorgang_baustein",Sys.Date(),".txt"),sep = "\t",row.names = FALSE)
+
+dataset <- fread(paste0(getwd(),"/output/antrag_vorgang_baustein2018-08-23.txt"))
+
+dataset <- read.xlsx(paste0(getwd(),"/output/antrag_vorgang_baustein2018-08-23.xlsx"),sheetIndex = 1)
--- r_code/toSet.R
+++ r_code/toSet.R
@@ -4,6 +4,7 @@
 
 ds1 <- inner_join(antrag, vorgang, by = "VorgangsNummer")
 
+ds2 <- left_join(ds1, baustein, by = "AntragsNummer")
 
 
 glimpse(ds1)
