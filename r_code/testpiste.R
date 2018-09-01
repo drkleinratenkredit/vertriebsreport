f6ed256aa387e87dfb5b6bb11d63d74768615ac6
VolkerInTheField <infoatmich@gmail.com>
2018-09-01 17:09:51 +0200

    Kennzahlen aus 'vorgang' genauer herausgearbeitet

--- r_code/testpiste.R
+++ r_code/testpiste.R
@@ -5,8 +5,14 @@ write.table(antrag,paste0(getwd(),"/output/antrag",Sys.Date(),".txt"),sep = "\t"
 
 write.table(vorgang_antrag,paste0(getwd(),"/output/vorgang_antrag",Sys.Date(),".txt"),sep = "\t",row.names = FALSE)
 
+write.table(ds0,paste0(getwd(),"/output/ds0_",Sys.Date(),".txt"),sep = "\t",row.names = FALSE)
+
 write.table(ds1,paste0(getwd(),"/output/ds1_",Sys.Date(),".txt"),sep = "\t",row.names = FALSE)
 
 
+a <- as_date("2018-08-22")
 
-
+b <- day(a)
+c <- wday(a,label = TRUE)
+d <- month(a)
+e <- paste0("Wochentag ",c)
